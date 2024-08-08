b4cf749bd9002347da59f63ce3e98590f4c83edd
Pc-Akkaracha <akkaracha555123@gmail.com>
2024-08-08 19:00:16 +0700

    complete upload post image fuctionality

--- Client/src/pages/CreatePost/CreatePost.jsx
+++ Client/src/pages/CreatePost/CreatePost.jsx
@@ -1,4 +1,14 @@
 import { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../../../firebase";
+// component
 import { Input } from "@/components/ui/input";
 import {
   Select,
@@ -10,13 +20,59 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { Button } from "@/components/ui/button";
-import ReactQuill from "react-quill";
-import "react-quill/dist/quill.snow.css";
+import { Alert, AlertDescription } from "@/components/ui/alert";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
 function CreatePost() {
   const [value, setValue] = useState("");
+
+  const [file, setFile] = useState(null);
+  const [imageUploadProgress, setImageUploadPrograss] = useState(null);
+  const [imageUploadError, setImageUploadError] = useState(null);
+
+  const [formData, setFormData] = useState({});
+  const handleUploadImage = async () => {
+    try {
+      if (!file) {
+        setImageUploadError("Please select an image");
+        return;
+      }
+      setImageUploadError(null);
+
+      const storage = getStorage(app);
+      const fileName = new Date().getTime() + "-" + file.name;
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          setImageUploadPrograss(progress.toFixed(0));
+        },
+        (error) => {
+          setImageUploadError("Image upload failed");
+          setImageUploadPrograss(null);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((dowloadURL) => {
+            setImageUploadError(null);
+            setImageUploadPrograss(null);
+            setFormData({ ...formData, image: dowloadURL });
+          });
+        }
+      );
+    } catch (error) {
+      setImageUploadError("Image upload failed");
+      setImageUploadPrograss(null);
+      console.log(error);
+    }
+  };
+
   return (
-    <div className="h-screen md:container">
+    <div className="h-auto md:container">
       <div className="text-center h-auto p-4 sm:my-4">
         <h1 className="text-5xl font-bold my-7">Crate a Post</h1>
         <form action="">
@@ -52,10 +108,38 @@ function CreatePost() {
                   type="file"
                   accepts="image/*"
                   className="bg-white text-black"
+                  onChange={(e) => setFile(e.target.files[0])}
                 />
               </div>
-              <Button className="">Upload image</Button>
+              <Button
+                onClick={handleUploadImage}
+                disabled={imageUploadProgress}
+                className="h-auto"
+              >
+                {imageUploadProgress ? (
+                  <div className="w-16 h-16">
+                    <CircularProgressbar
+                      value={imageUploadProgress}
+                      text={`${imageUploadProgress || 0}%`}
+                    ></CircularProgressbar>
+                  </div>
+                ) : (
+                  "Upload Imgae"
+                )}
+              </Button>
             </div>
+            {imageUploadError && (
+              <Alert variant="destructive">
+                <AlertDescription>{imageUploadError}</AlertDescription>
+              </Alert>
+            )}
+            {formData.image && (
+              <img
+                src={`${formData.image}`}
+                alt=""
+                className="w-full h-72 object-cover"
+              />
+            )}
             {/* section 3 */}
 
             <ReactQuill
