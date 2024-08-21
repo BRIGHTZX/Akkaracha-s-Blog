34809a5b648f8bba63857af87a12647b51cc48fa
Pc-Akkaracha <akkaracha555123@gmail.com>
2024-08-21 22:14:07 +0700

    fix index.js

--- api/index.js
+++ api/index.js
@@ -6,7 +6,6 @@ import authRoutes from "./routes/auth.route.js";
 import postRoutes from "./routes/post.route.js";
 import commentRoutes from "./routes/comment.route.js";
 import cookieParser from "cookie-parser";
-import cors from "cors";
 import path from "path";
 
 dotenv.config();
@@ -14,59 +13,40 @@ dotenv.config();
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
-    console.log("Mongo is connected");
+    console.log("MongoDb is connected");
   })
-  .catch((error) => {
-    console.log(error);
+  .catch((err) => {
+    console.log(err);
   });
 
 const __dirname = path.resolve();
+
 const app = express();
-
-// CORS middleware
-app.use(
-  cors({
-    origin: ["https://akkarachas-blog.onrender.com", "http://localhost:5173"], // กำหนดโดเมนที่อนุญาต
-    methods: ["GET", "POST", "PUT", "DELETE"], // กำหนดวิธีการที่อนุญาต
-    credentials: true, // เปิดใช้งานการส่งคุกกี้และข้อมูลการรับรองอื่นๆ
-  })
-);
-
-// // Middleware for setting COEP and CORS headers
-// app.use((req, res, next) => {
-//   res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
-//   res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
-//   next();
-// });
 
 app.use(express.json());
 app.use(cookieParser());
 
-// API Routes
+app.listen(4000, () => {
+  console.log("Server is running on port 4000!");
+});
+
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/comment", commentRoutes);
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, "Client", "dist")));
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
-// Handle all other routes with the React app
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "Client", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 
-// Control status and error
-app.use((error, req, res, next) => {
-  const statusCode = error.statusCode || 500;
-  const message = error.message || "Internal Server Error";
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
   res.status(statusCode).json({
     success: false,
     statusCode,
     message,
   });
-});
-
-app.listen(4000, () => {
-  console.log("Server running on port 4000");
 });
